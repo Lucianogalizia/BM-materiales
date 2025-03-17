5eb2c1197207cc999b622224a748e27ca71736cf
Lucianogalizia <lucianogalizia170@hotmail.com>
2025-03-17 11:13:13 -0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-// static/main.js
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("¡Bienvenido a la aplicación interactiva!");
-    // Aquí puedes agregar más lógica JavaScript para mejorar la interactividad
-});
